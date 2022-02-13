5a6046000870b03ddeaa86d93ce06c2c5e49e03d
Nazmul Hassan <nazmul.phpdroidx@gmail.com>
2022-02-13 19:41:14 +0600

    storage module imported

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { StorageModule } from '../storage/storage.module';
 import { FileSchema } from '../entities/file.entity';
 import { FileService } from './file.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: File.name, schema: FileSchema }]),
+    StorageModule,
   ],
   providers: [FileService]
 })
